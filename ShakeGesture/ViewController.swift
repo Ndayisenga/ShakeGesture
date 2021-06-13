e3c639c00eb6ade2333860c2e3919ea3e4a92969
Ndayisenga Jean Claude <cloude056@yahoo.fr>
2021-06-13 14:02:26 +0200

    Initial Commit

---
+++ ShakeGesture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShakeGesture
+//
+//  Created by Ndayisenga Jean Claude on 13/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
